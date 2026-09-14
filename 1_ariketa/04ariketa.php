21c2692d3f0d9ded5e0b5d38ac620799a99b2c54
aiza-fp <aiza@fpzornotza.com>
2026-09-14 11:03:05 +0200

    zuzendu ordenazioa

--- 1_ariketa/04ariketa.php
+++ 1_ariketa/04ariketa.php
@@ -1,7 +1,7 @@
 <?php
-$z1 = 9;
-$z2 = 6;
-$z3 = 6;
+$z1 = -50;
+$z2 = -43;
+$z3 = -43;
 
 if($z1 >= $z2 && $z2 >= $z3){
     echo "<br>" . "1-Ordenatuta beherantz:" . $z1 .  " " . $z2 .  " " . $z3 .  " ";
@@ -9,7 +9,7 @@ if($z1 >= $z2 && $z2 >= $z3){
 }elseif($z1 >= $z2 && $z2 < $z3 && $z1 >= $z3){
     echo "<br>" . "2-Ordenatuta beherantz: " . $z1 .  " " . $z3 .  " " . $z2 .  " ";
     echo "<br>" . "2-Ordenatuta gorantz: " . $z2 .  " " . $z3 .  " " . $z1 .  " ";
-}elseif($z1 >= $z2 && $z2S < $z3 && $z1 < $z3){
+}elseif($z1 >= $z2 && $z2 < $z3 && $z1 < $z3){
     echo "<br>" . "3-Ordenatuta beherantz: " . $z3 .  " " . $z1 .  " " . $z2 .  " ";
     echo "<br>" . "3-Ordenatuta gorantz: " . $z2 .  " " . $z1 .  " " . $z3 .  " ";
 }
@@ -25,15 +25,15 @@ elseif($z2 >= $z3 && $z3 >= $z1){
     echo "<br>" . "6-Ordenatuta gorantz: " . $z3 .  " " . $z2 .  " " . $z1 .  " ";
 }
 
-elseif($z2 >= $z3 && $z3 >= $z1){
-    echo "<br>" . "7-Ordenatuta beherantz:" . $z2 .  " " . $z3 .  " " . $z1 .  " ";
-    echo "<br>" . "7-Ordenatuta gorantz:" . $z1 .  " " . $z3 .  " " . $z2 .  " ";
-}elseif($z2 >= $z3 && $z3 < $z1 && $z2 >= $z1){
-    echo "<br>" . "8-Ordenatuta beherantz: " . $z2 .  " " . $z1 .  " " . $z3 .  " ";
-    echo "<br>" . "8-Ordenatuta gorantz: " . $z3 .  " " . $z1 .  " " . $z2 .  " ";
-}elseif($z2 >= $z3 && $z3 < $z1 && $z2 < $z1){
-    echo "<br>" . "9-Ordenatuta beherantz: " . $z1 .  " " . $z2 .  " " . $z3 .  " ";
-    echo "<br>" . "9-Ordenatuta gorantz: " . $z3 .  " " . $z2 .  " " . $z1 .  " ";
+elseif($z3 >= $z1 && $z1 >= $z2){
+    echo "<br>" . "7-Ordenatuta beherantz:" . $z3 .  " " . $z1 .  " " . $z2 .  " ";
+    echo "<br>" . "7-Ordenatuta gorantz:" . $z2 .  " " . $z1 .  " " . $z3 .  " ";
+}elseif($z3 >= $z1 && $z1 < $z2 && $z3 >= $z2){
+    echo "<br>" . "8-Ordenatuta beherantz: " . $z3 .  " " . $z2 .  " " . $z1 .  " ";
+    echo "<br>" . "8-Ordenatuta gorantz: " . $z1 .  " " . $z2 .  " " . $z3 .  " ";
+}elseif($z3 >= $z1 && $z1 < $z2 && $z3 < $z2){
+    echo "<br>" . "9-Ordenatuta beherantz: " . $z2 .  " " . $z3 .  " " . $z1 .  " ";
+    echo "<br>" . "9-Ordenatuta gorantz: " . $z1 .  " " . $z3 .  " " . $z2 .  " ";
 }
 
 ?>
