1a16a9a9946bf78efa479e03b60d81018f6aa3d2
aiza-fp <aiza@fpzornotza.com>
2026-09-15 11:16:27 +0200

    frogak

--- frogak/datuak.php
+++ frogak/datuak.php
@@ -5,6 +5,7 @@ $arraya = ["Ane" => 9, "Jon" => 8, "Maria" => 7];
 var_dump($arraya);
 $kate1 = "Kaixo";
 $kate2 = "Mundua";
-echo "<br>" . $kate1 . " " . $kate2;
+echo "<br>" . $kate1 . " " . $kate2 . " ";
+echo size($arraya);
 
 ?>
